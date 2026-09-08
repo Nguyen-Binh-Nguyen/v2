d1bdc351861b64c53063274965ad2b0d7b1a868a
Nguyen-Binh-Nguyen <amygirl0312@gmail.com>
2026-09-08 17:55:10 +0700

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'class',
   theme: {
-    extend: {
-      colors: {
-        blue: {
-          950: '#172554',
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
